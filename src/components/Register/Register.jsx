cf529c826a888794eac5697602be200afd34e454
Githmin Jayawardhana <Githmin21@gmail.com>
2023-06-09 16:52:10 +0530

    added register page basic functionality

---
+++ src/components/Register/Register.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import "./Register.css";
+import Btn from "../Buttons/Btn";
+import axios from "axios";
+
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+
+const Register = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [rePassword, setRePassword] = useState("");
+
+  const handelSignup = () => {
+    if (password === rePassword) {
+      const instance = axios.create({
+        withCredentials: true,
+        baseURL: props.host,
+      });
+
+      instance
+        .post("/api/auth/register", {
+          email,
+          password,
+        })
+        .then(
+          (response) => {
+            console.log(response);
+            if (response.status === 200) {
+              toast.success("Signup Successfull");
+            }
+          },
+          (error) => {
+            toast.error("Signup Unsuccessfull");
+          }
+        );
+    }
+  };
+
+  return (
+    <div
+      className="
+    formMain"
+    >
+      <div className="formInner">
+        <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+        <input
+          placeholder="Password"
+          type="password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <input
+          placeholder="Re-enter password"
+          type="password"
+          className={password === rePassword ? "match" : "notMatch"}
+          onChange={(e) => setRePassword(e.target.value)}
+        />
+        <div className="errorArea">
+          {password === rePassword ? "" : "PASSWORDS DO NOT MATCH"}
+        </div>
+      </div>
+      <Btn name={"SIGN UP"} onClickProp={handelSignup} />
+      <ToastContainer
+        position="top-right"
+        autoClose={10000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
+    </div>
+  );
+};
+
+export default Register;
